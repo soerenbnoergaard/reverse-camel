22e5da864d1f55c7e2aeb13a4779c6567cc6076e
Søren Bøgeskov Nørgaard <soerenbnoergaard@gmail.com>
2020-04-15 22:52:11 +0200

    Release 0.0.1 for Linux

---
+++ Makefile
@@ -0,0 +1,7 @@
+.PHONY: all clean
+
+all:
+	$(MAKE) -C src/reverse-camel/
+
+clean:
+	$(MAKE) -C src/reverse-camel clean
--- src/reverse-camel/ReverseCamel.cpp
+++ src/reverse-camel/ReverseCamel.cpp
@@ -194,7 +194,7 @@ protected:
     */
     uint32_t getVersion() const override
     {
-        return d_version(0, 0, 0);
+        return d_version(0, 0, 1);
     }
 
    /**
